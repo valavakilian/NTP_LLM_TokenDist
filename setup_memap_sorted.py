eb9602229070a2080cb11f62e804c17855785950
valavakilian <mahvaala@yahoo.com>
2024-10-02 13:11:05 -0700

    removed some older files

--- setup_memap_sorted.py
+++ setup_memap_sorted.py
@@ -7,7 +7,7 @@ setup(
     ext_modules=[
         CppExtension(
             name='trie_module_memap_sorted',
-            sources=['/arc/project/st-cthrampo-1/vala/NTP_LLM_TokenDist_Wiki_childDict/trie_module_memap_sorted.cpp'],  # Correct file name
+            sources=['/arc/project/st-cthrampo-1/vala/NTP_LLM_tokenDist_Wiki/trie_module_memap_sorted.cpp'],  # Correct file name
         ),
     ],
     cmdclass={
